b220208d385b740e826769991754370d11517594
Ben Jacoby <bjac6790@gmail.com>
2018-12-04 20:34:59 -0500

    Adding in functionality to view all files, and to add untracked files.

--- gitCommandLine2/ConsoleApp2/DesktopApp/MainWindow.xaml.cs
+++ gitCommandLine2/ConsoleApp2/DesktopApp/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace DesktopApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        GitConnected gc;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,8 +29,47 @@ namespace DesktopApp
         
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            GitConnected gc = new GitConnected(GitPath.Text);
+            gc = new GitConnected(GitPath.Text);
+            GitPath.IsReadOnly = true;
             Output.Text = gc.gitCommand("status");
+            SetDir.Visibility = Visibility.Hidden;
+            GitPath.Foreground = new SolidColorBrush(Colors.DarkGray);
+            GitPath.Background = new SolidColorBrush(Colors.LightGray);
+            Directory.Text = gc.gitCommand("ls-files -t") + gc.gitCommand("ls-files --other");
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            RunUpdate();
+        }
+
+        private void RunUpdate()
+        {
+            Output.Text = gc.gitCommand("status"); 
+            
+            // Check for untracked files.
+            string toAdd = gc.gitCommand("ls-files --other");
+            if ("" != toAdd)
+            {
+                AddAll.Visibility = Visibility.Visible;
+            }
+            
+            // Check for files with changes.
+            string toCommit = gc.gitCommand("ls-files --modified");
+            Directory.Text = gc.gitCommand("ls-files -t") + toAdd;
+            
+        }
+
+        private void GitPath_GotFocus(object sender, RoutedEventArgs e)
+        {
+            GitPath.Text = "";
+        }
+
+        private void AddAll_Click(object sender, RoutedEventArgs e)
+        {
+            gc.gitCommand("add .");
+            AddAll.Visibility = Visibility.Hidden;
+            RunUpdate();
         }
     }
 }
